15a062806797e9ea8bac88afac4d53cabbe1668f
Lukasz Mentel <lmmentel@gmail.com>
2018-01-19 15:06:21 +0100

    updated the dmg builder

--- utils/buildosxapp.sh
+++ utils/buildosxapp.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-PROJECT_ROOT=../
-IMAGE_PATH=../images
-IMAGE=BatchCalculatorApp.dmg
-APP=BatchCalculatorApp
-APP_PATH=$PROJECT_ROOT/buildapp
 
-mkdir $APP_PATH
 
-cp $IMAGE_PATH/$IMAGE $APP_PATH
-cd $APP_PATH
+PROJECT_ROOT=/Users/lukaszmentel/Devel/batchcalculator
+IMAGE_FILE=BatchCalculator.dmg
+APP_NAME=BatchCalculator
+WORKDIR=$PROJECT_ROOT/utils/buildapp
 
-hdiutil convert $IMAGE -format UDSP -o $APP
+mkdir -p $WORKDIR
+cd $WORKDIR
 
-rm $IMAGE 
+# create the image containing the app
+hdiutil create -size 50m -fs HFS+ -srcfolder $PROJECT_ROOT/dist/BatchCalculator.app -volname $APP_NAME $IMAGE_FILE
+# convert to sparseimage
+hdiutil convert $IMAGE_FILE -format UDSP -o $APP_NAME
+rm $IMAGE_FILE
 
-hdiutil attach ${APP}.sparseimage
+# create the shortcut to Applications folder
+hdiutil attach ${APP_NAME}.sparseimage
+cd /Volumes/$APP_NAME
+ln -s /Applications Applications
+cd $WORKDIR
+hdiutil detach /Volumes/$APP_NAME
 
-cp -r $PROJECT_ROOT/dist/batchcalc.app/Contents /Volumes/$APP/BatchCalculator.app/Contents
-
-hdiutil detach /Volumes/$APP
-
-hdiutil convert ${APP}.sparseimage -format UDBZ -o ${APP}.dmg
-rm ${APP}.sparseimage
+# convert sparseimage to dmg and cleanup 
+hdiutil convert ${APP_NAME}.sparseimage -format UDBZ -o ${APP_NAME}.dmg
+rm ${APP_NAME}.sparseimage
